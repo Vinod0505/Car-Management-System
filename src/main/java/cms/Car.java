3001c7b6da0866c6be33646da9fe0242dee8030d
vinod0505 <vinodbojaraju@gmail.com>
2024-06-20 15:14:42 +0530

    created the project with entity

---
+++ src/main/java/cms/Car.java
@@ -0,0 +1,39 @@
+package cms;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+
+	@Id
+	private int carId;
+	private String carName;
+	private String carModel;
+	private int carPrice;
+	public int getCarId() {
+		return carId;
+	}
+	public void setCarId(int carId) {
+		this.carId = carId;
+	}
+	public String getCarName() {
+		return carName;
+	}
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
+	public String getCarModel() {
+		return carModel;
+	}
+	public void setCarModel(String carModel) {
+		this.carModel = carModel;
+	}
+	public int getCarPrice() {
+		return carPrice;
+	}
+	public void setCarPrice(int carPrice) {
+		this.carPrice = carPrice;
+	}
+	
+}
